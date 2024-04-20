8b1c078b75c402f00a1cc0ba7c76554923127b2d
Nvm35 <twikk3@gmail.com>
2024-04-20 16:20:15 +0500

    oi

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -38,7 +38,13 @@ export default function App() {
           />
           <Stack.Screen
             name='FoodScreen'
-            component={FoodScreen} />
+            component={FoodScreen}
+          // options={{
+          //   headerRight: () => {
+          //     return <Button title='tap' />
+          //   }
+          // }} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -3,13 +3,25 @@ import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
+import { useLayoutEffect } from 'react';
+import IconButton from '../components/IconButton';
 
 
-function FoodScreen({ route }) {
+function FoodScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId)
-  console.log(selectedMeal)
+  function headerPress() {
+    console.log(mealId)
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <IconButton icon='star' color='white' onPress={headerPress} />
+      }
+    })
+  }, [navigation, headerPress])
 
 
   return (
