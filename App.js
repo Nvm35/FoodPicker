1b8a6e9bca18d6ed8a2a4c878c05bdd153d09d00
Nvm35 <twikk3@gmail.com>
2024-04-22 16:42:42 +0500

    fin

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Provider } from 'react-redux';
 
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsScreen from './screens/MealsScreen';
 import FoodScreen from './screens/FoodScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
-import FavoritesContextProvider from './store/context/favorites-context';
+import { store } from './store/redux/store';
 
 
 const Stack = createNativeStackNavigator();
@@ -57,7 +58,8 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
 
         <NavigationContainer>
           <Stack.Navigator screenOptions={{
@@ -99,7 +101,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
 
     </>
   );
--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -3,23 +3,29 @@ import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
-import { useContext, useLayoutEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import IconButton from '../components/IconButton';
-import { FavoritesContext } from '../store/context/favorites-context';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite, removeFavorite } from '../store/redux/favorites'
+
 
 
 function FoodScreen({ route, navigation }) {
-  const favMealsCtx = useContext(FavoritesContext)
+  // const favMealsCtx = useContext(FavoritesContext)
+  const favMealIds = useSelector((state) => state.favoriteMeals.ids);
+  const dispatch = useDispatch();
   const mealId = route.params.mealId;
-  const mealFavorite = favMealsCtx.ids.includes(mealId);
+  const mealFavorite = favMealIds.includes(mealId);
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId)
 
   function changeFavStat() {
     if (mealFavorite) {
-      favMealsCtx.removeFavorite(mealId)
+      // favMealsCtx.removeFavorite(mealId)
+      dispatch(removeFavorite({ id: mealId }))
     } else {
-      favMealsCtx.addFavorite(mealFavorite)
+      // favMealsCtx.addFavorite(mealFavorite)
+      dispatch(addFavorite({ id: mealId }))
     }
   }
 
